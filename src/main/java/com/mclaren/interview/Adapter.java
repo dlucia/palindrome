db2386c8e4bc0653ca55937de506f6571c151a30
Donato Lucia <donato.lucia@lastminute.com>
2017-12-10 21:49:27 +0100

    more clear name

--- src/main/java/com/mclaren/interview/Adapter.java
+++
@@ -1,6 +0,0 @@
-package com.mclaren.interview;
-
-public interface Adapter<IN, OUT>
-{
-  OUT adapt(IN palindromes);
-}
--- src/main/java/com/mclaren/interview/Validator.java
+++
@@ -1,6 +0,0 @@
-package com.mclaren.interview;
-
-public interface Validator<VALUE>
-{
-  void validate(VALUE value);
-}
